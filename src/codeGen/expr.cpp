f1267f19499051710fb4a2ae3262a109a05bd2c2
Eric Morris <1210571447@qq.com>
2019-06-04 21:31:49 +0800

    writ改了但是segfault

--- src/codeGen/expr.cpp
+++ src/codeGen/expr.cpp
@@ -134,10 +134,12 @@ namespace yapc {
                     func_args.push_back(value);
                 }
                 else if (value->getType()->isArrayTy()) {
-                    auto real_arg = std::dynamic_pointer_cast<IdentifierAST>(arg);
-                    auto *value2 = real_arg->GetPtr(context);
-                    std::string mystr2 = value2->getName().str();
-                    func_args.push_back(context.GetBuilder().CreateGlobalStringPtr(mystr2));
+                    func_args.push_back(context.GetBuilder().CreateGlobalStringPtr("%s"));
+                    func_args.push_back(value);
+                    //auto real_arg = std::dynamic_pointer_cast<IdentifierAST>(arg);
+                    //auto *value2 = real_arg->GetPtr(context);
+                    //std::string mystr2 = value2->getName().str();
+                    //func_args.push_back(context.GetBuilder().CreateGlobalStringPtr(mystr2));
                 }
                  //   else if (value->getType()->)
                 // TODO: string support
